1e5ca08acb600078711b69081b493d9ab1002765
Anton Surkis <antonsurkis@gmail.com>
2020-11-06 08:49:02 +0300

    Added more BMPs for tests

--- src/main.c
+++ src/main.c
@@ -3,19 +3,26 @@
 #include "log.h"
 #include <stdio.h>
 
-int main() {
+int main(int argc, char **argv) {
   struct ImageRgb8 src;
   struct ImageRgb8 dst;
-  from_bmp_path(&src, "input.bmp");
+
+  if (argc < 3) {
+    log_err("Not enough arguments");
+    return __LINE__;
+  }
+
+  from_bmp_path(&src, argv[1]);
 
   /* BMP сохраняет изображения в BGR и снизу вверх */
-  image_bgr_to_rgb(&src);
+  /* if (image_bgr_to_rgb(&src)) return __LINE__;
   image_mirror_v(&src);
-  image_rotate_clockwise(&src, &dst);
+  if (image_rotate_clockwise(&src, &dst)) return __LINE__;
 
-  image_bgr_to_rgb(&dst);
-  image_mirror_v(&dst);
-  to_bmp_path(&dst, "output.bmp");
+  if (image_bgr_to_rgb(&dst)) return __LINE__;
+  image_mirror_v(&dst); */
+  if (image_rotate_counter_clockwise(&src, &dst)) return __LINE__;
+  if (to_bmp_path(&dst, argv[2])) return __LINE__;
 
   destroy_image(&src);
   destroy_image(&dst);
